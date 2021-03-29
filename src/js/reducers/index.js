125d47df55cc862ccc658aa30bb51cd5c2fb1cf9
Nevo067 <51850812+Nevo067@users.noreply.github.com>
2021-03-29 14:59:14 +0200

    Update reducer by a switch

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,11 +1,40 @@
-import ADD_MESSAGE, { MESSAGE_LOAD, MESSAGE_LOAD_FRONT } from '../constant/index';
+import { act } from 'react-dom/test-utils';
+import ADD_MESSAGE, { MESSAGE_LOAD, MESSAGE_LOAD_FRONT,COMPTE_LOAD, COMPTE_RESET } from '../constant/index';
 
 const initialState = {
-    messages: []
+    messages: [],
+    comptes:[]
 };
 
 function rootReducer(state = initialState,action)
 {
+    switch (action.type) {
+        case ADD_MESSAGE:
+            console.log("message se lance")
+            return Object.assign({}, state, {
+                messages: [...state.messages].concat(action.payload)
+            });
+        case MESSAGE_LOAD:
+            console.log("cela marche")
+            return {
+                ...state,
+                messages: state.messages.concat(action.payload)
+            }
+        case COMPTE_LOAD:
+            return {
+                ...state,
+                comptes: state.comptes.concat(action.payload)
+            }
+        case COMPTE_RESET:
+            return {
+                ...state,
+                comptes: []
+            }
+        default:
+            break;
+    }
+
+    /*
     if(action.type == ADD_MESSAGE)
     {
         console.log("message se lance")
@@ -13,7 +42,7 @@ function rootReducer(state = initialState,action)
             messages: [...state.messages].concat(action.payload)
         });
     }
-    if(action.type == MESSAGE_LOAD)
+    else if(action.type == MESSAGE_LOAD)
     {
         console.log("cela marche")
         return{
@@ -21,7 +50,23 @@ function rootReducer(state = initialState,action)
              messages: state.messages.concat(action.payload) 
         } 
     }
+    else if(action.type == COMPTE_LOAD)
+    {
+        
+        return{
+            ...state,
+            comptes:state.comptes.concat(action.payload)
+        }
+    }
+    else if(action.type == COMPTE_RESET)
+    {
+        return{
+            ...state,
+            comptes:[]
+        }
+    }
     return state;
+    */
 };
 
 export default rootReducer
