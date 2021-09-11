6ce0de48d34f27c377a50b1c21ee5f6e6b478b41
Nevo067 <51850812+Nevo067@users.noreply.github.com>
2021-09-11 14:50:15 +0200

    update css to mobile

--- src/Component/MessageComponent/CompteComponent/OneCompteComponent/OneCompteComponent.js
+++ src/Component/MessageComponent/CompteComponent/OneCompteComponent/OneCompteComponent.js
@@ -5,17 +5,40 @@ class OneCompteComponent extends Component
     constructor(props)
     {
         super(props)
+        this.state = {isEdit:true,nom:""}
+        
     }
     render()
     {
+        let isEdit = this.state.isEdit; 
         return(
-            <div class ="row  compte" onClick = {()=>this.props.changeConv(this.props.compteId)}>
-                <p>{this.props.compteLogin}</p><br/>
-                
+            <div class="row">
+                {isEdit 
+                    ? 
+                <div class ="col col-xl compte">
+                    <p onClick = {()=>this.props.changeConv(this.props.compteId)}>
+                        {this.props.compteLogin}
+                    </p>
+                </div>
+                :
+                <div class="col col-xl compte">
+                    <label for ="nom"></label>
+                    <input type="text" name="nom" onChange={(e)=>this.changeMessage(e)}></input>
+                </div>   
+                }
+                <button class="btn btn-block btn-warning col-3 border" onClick={()=>this.changeIsEdit()}>Edition</button>
             </div>
-
         )
         
+    }
+    changeIsEdit()
+    {
+        let isEdit = this.state.isEdit;
+        this.setState({isEdit : !isEdit})
+    }
+    changeMessage(e)
+    {
+        this.setState({nom:e.target.value})
     }
 }
 export default OneCompteComponent;
